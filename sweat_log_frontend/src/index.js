1f7260dee45fdc77aed548b4ff2e9feb64266a0e
dnodolf1212 <dnodolf@gmail.com>
2020-11-25 21:44:02 -0800

    set yp service class and a components directory . .

--- sweat_log_frontend/src/index.js
+++ sweat_log_frontend/src/index.js
@@ -1,3 +1,7 @@
-document.addEventListener(() => {
+const BASE_URL = "http://localhost:3000/"
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("We good!")
 
-}
+  const api = new ApiService("http://localhost:3000");
+  console.log(api)
+})
